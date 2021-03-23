0a68bec06512e99d416699057c254aab7be2a8c9
Venture <tido.squat@gmail.com>
2021-03-23 18:30:56 +0100

    create two ways for inscription

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -27,6 +27,7 @@ class LoginController extends AbstractController
 
     /**
      * @Route("/inscription", name="inscription")
+     * @Route("/admin/inscription", name="inscription")
      */
     public function inscription(UserPasswordEncoderInterface $encoder, EntityManagerInterface $manager, Request $request): Response
     {
