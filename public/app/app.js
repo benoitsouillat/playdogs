9f3fa028ae42986ab1cf2ba6c4b67837e960c82d
Venture <tido.squat@gmail.com>
2021-03-24 18:16:57 +0100

    responsive header and price done -- togglebutton done

--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,44 @@
 let menuButton = document.getElementById('menu-btn');
 let menuList = document.getElementById('menu-list');
 
+let btnStyle = window.getComputedStyle(menuButton);
+let listStyle = window.getComputedStyle(menuList);
+
+if (listStyle.display === 'flex')
+{
+    menuButton.textContent = "Fermer";
+}
+else 
+{
+    menuButton.textContent = " Menu ";
+}
+
+const toggleMenu = () => {
+
+    if(menuButton.textContent === "Fermer")
+    {
+        menuList.style.display = "none";
+        menuButton.textContent = " Menu ";
+    }
+    else
+    {
+        menuList.style.display = "flex";
+        menuButton.textContent = "Fermer";
+    }
+}
+
+let theTimer;
+window.addEventListener('resize', function () {
+    clearTimeout(theTimer);
+    theTimer = setTimeout(function () {
+        if (window.innerWidth > 550) {
+            menuList.style.display = "flex"
+        }
+        else if (window.innerWidth <= 550) {
+            menuList.style.display = "none";
+            menuButton.textContent = " Menu ";
+        }
+    }, 75)
+});
+
+menuButton.addEventListener("click", toggleMenu);
