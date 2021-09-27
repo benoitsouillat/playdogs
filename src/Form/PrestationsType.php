dcd536686dbcbe57a45018bb545f7c307b5d1f89
Venture <tido.squat@gmail.com>
2021-09-27 16:40:11 +0200

    start form of prestations

--- src/Form/PrestationsType.php
+++ src/Form/PrestationsType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Prestations;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PrestationsType extends AbstractType
@@ -12,10 +14,14 @@ class PrestationsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('description')
+            ->add('description', TextareaType::class,  [
+                'label' => 'Description de la prestation ',
+
+            ])
             ->add('price')
-            ->add('groomer')
-        ;
+            ->add('groomer', TextType::class, [
+                'label' => 'Toiletteur(se) ',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
